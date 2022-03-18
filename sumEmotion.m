45aeba5eb0187d6963030dacc91fc462c2b0d3eb
ynajmanl <101124174+ynajmanl@users.noreply.github.com>
2022-03-18 09:43:34 -0400

    Create sumEmotion
    function sums emotional valency of text

---
+++ sumEmotion.m
@@ -0,0 +1,12 @@
+function [countEmotion] = sumEmotion(bagTbl,)
+    countEmotion = zeros(1,10);
+    table_size = bagTbl.size();
+    rows = table_size(1);
+    for i = 1:rows
+        row = bagTbl(i,:);
+        word = bagTbl(i:1);
+        if isequal(word, )
+
+        end
+    end
+end
