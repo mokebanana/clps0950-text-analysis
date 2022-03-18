ef7801fa43f5f6b68060807435c15d46bf7564ec
ynajmanl <101124174+ynajmanl@users.noreply.github.com>
2022-03-18 10:44:28 -0400

    Figure(2)
    word cloud for the words

--- makeGraphs.m
+++ makeGraphs.m
@@ -6,3 +6,8 @@ subplot(1,2,1)
 barPosNeg = bar(posNeg);
 subplot(1,2,2)
 barSentiments = bar(sentiments);
+
+figure(2)
+uniqueWords = ["Positive", "Negative", "Anger", "Anticipation", "Digust", "Fear", "Joy", "Sadness", "Surprise", "Trust"];
+bag = bagOfWords(uniqueWords, emotionsVec);
+wordcloud(bag);
