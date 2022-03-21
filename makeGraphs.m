dc9833616e0e2af3dd52506b4f307e62df78842a
ynajmanl <101124174+ynajmanl@users.noreply.github.com>
2022-03-20 22:42:30 -0400

    Titles and Axes
    Added titles and axes to graphs

--- makeGraphs.m
+++ makeGraphs.m
@@ -4,8 +4,12 @@ sentiments = emotionsVec(3:10);
 figure(1)
 subplot(1,2,1)
 barPosNeg = bar(posNeg);
+title('Total Pos. and Neg. Valency of Words');
+ylabel('# of Words')
 subplot(1,2,2)
 barSentiments = bar(sentiments);
+title('Total Valency of Dif. Emotions');
+ylabel('# of Words');
 
 figure(2)
 uniqueWords = ["Positive", "Negative", "Anger", "Anticipation", "Digust", "Fear", "Joy", "Sadness", "Surprise", "Trust"];
